65f76d0baf19c82208aac6233ba364889d762963
KhachLaptop <khach.obt@gmail.com>
2020-02-19 14:37:01 -0500

    RestApi

--- YTAPItest1/Program.cs
+++ YTAPItest1/Program.cs
@@ -1,103 +1,118 @@
 ﻿using System;
-using System.IO;
-using System.Reflection;
-using System.Threading;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Services;
-using Google.Apis.Upload;
-using Google.Apis.Util.Store;
-using Google.Apis.YouTube.v3;
-using Google.Apis.YouTube.v3.Data;
-
-namespace Google.Apis.YouTube.Samples
+namespace HttpClientSample
 {
-    /// <summary>
-    /// YouTube Data API v3 sample: retrieve my uploads. +
-    /// Relies on the Google APIs Client Library for .NET, v1.7.0 or higher.
-    /// See https://developers.google.com/api-client-library/dotnet/get_started
-    /// </summary>
-    internal class MyUploads
+    public class Product
     {
-        [STAThread]
-        static void Main(string[] args)
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+
+        static void ShowProduct(Product product)
         {
-            Console.WriteLine("YouTube ++ Data API: My Uploads");
-            Console.WriteLine("============================");
+            Console.WriteLine($"Name: {product.Name}\tPrice: " +
+                $"{product.Price}\tCategory: {product.Category}");
+        }
+
+        static async Task<Uri> CreateProductAsync(Product product)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                "api/products", product);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+
+        static async Task<Product> GetProductAsync(string path)
+        {
+            Product product = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                product = await response.Content.ReadAsAsync<Product>();
+            }
+            return product;
+        }
+
+        static async Task<Product> UpdateProductAsync(Product product)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync(
+                $"api/products/{product.Id}", product);
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated product from the response body.
+            product = await response.Content.ReadAsAsync<Product>();
+            return product;
+        }
+
+        static async Task<HttpStatusCode> DeleteProductAsync(string id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync(
+                $"api/products/{id}");
+            return response.StatusCode;
+        }
+
+        static void Main()
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task RunAsync()
+        {
+            // Update port # in the following line.
+            client.BaseAddress = new Uri("http://localhost:64195/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
 
             try
             {
-                new MyUploads().Run().Wait();
-            }
-            catch (AggregateException ex)
-            {
-                foreach (var e in ex.InnerExceptions)
+                // Create a new product
+                Product product = new Product
                 {
-                    Console.WriteLine("Error: " + e.Message);
-                }
+                    Name = "Gizmo",
+                    Price = 100,
+                    Category = "Widgets"
+                };
+
+                var url = await CreateProductAsync(product);
+                Console.WriteLine($"Created at {url}");
+
+                // Get the product
+                product = await GetProductAsync(url.PathAndQuery);
+                ShowProduct(product);
+
+                // Update the product
+                Console.WriteLine("Updating price...");
+                product.Price = 80;
+                await UpdateProductAsync(product);
+
+                // Get the updated product
+                product = await GetProductAsync(url.PathAndQuery);
+                ShowProduct(product);
+
+                // Delete the product
+                var statusCode = await DeleteProductAsync(product.Id);
+                Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
 
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
-        }
-
-        private async Task Run()
-        {
-            UserCredential credential;
-            using (var stream = new FileStream("client_secrets.json", FileMode.Open, FileAccess.Read))
-            {
-                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
-                    // This OAuth 2.0 access scope allows for read-only access to the authenticated 
-                    // user's account, but not other types of account access.
-                    new[] { YouTubeService.Scope.YoutubeReadonly },
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore(this.GetType().ToString())
-                );
-            }
-
-            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = this.GetType().ToString()
-            });
-
-            var channelsListRequest = youtubeService.Channels.List("contentDetails");
-            channelsListRequest.Mine = true;
-
-            // Retrieve the contentDetails part of the channel resource for the authenticated user's channel.
-            var channelsListResponse = await channelsListRequest.ExecuteAsync();
-
-            foreach (var channel in channelsListResponse.Items)
-            {
-                // From the API response, extract the playlist ID that identifies the list
-                // of videos uploaded to the authenticated user's channel.
-                var uploadsListId = channel.ContentDetails.RelatedPlaylists.Uploads;
-
-                Console.WriteLine("Videos in list {0}", uploadsListId);
-
-                var nextPageToken = "";
-                while (nextPageToken != null)
-                {
-                    var playlistItemsListRequest = youtubeService.PlaylistItems.List("snippet");
-                    playlistItemsListRequest.PlaylistId = uploadsListId;
-                    playlistItemsListRequest.MaxResults = 50;
-                    playlistItemsListRequest.PageToken = nextPageToken;
-
-                    // Retrieve the list of videos uploaded to the authenticated user's channel.
-                    var playlistItemsListResponse = await playlistItemsListRequest.ExecuteAsync();
-
-                    foreach (var playlistItem in playlistItemsListResponse.Items)
-                    {
-                        // Print information about each video.
-                        Console.WriteLine("{0} ({1})", playlistItem.Snippet.Title, playlistItem.Snippet.ResourceId.VideoId);
-                    }
-
-                    nextPageToken = playlistItemsListResponse.NextPageToken;
-                }
-            }
+            Console.ReadLine();
         }
     }
 }
